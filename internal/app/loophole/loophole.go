53d8804d25540c97372fb5d0b1ba00f377b5c812
Denny Königsmann <denny@main.dev>
2020-11-16 10:36:57 +0100

    Fixed misleading variable names inside handleClient

--- internal/app/loophole/loophole.go
+++ internal/app/loophole/loophole.go
@@ -79,29 +79,29 @@ func parsePublicKey(file string) (ssh.AuthMethod, ssh.PublicKey, error) {
 }
 
 // From https://sosedoff.com/2015/05/25/ssh-port-forwarding-with-go.html
-// Handle local client connections and tunnel data to the remote serverq
+// Handle client connections and tunnel data to the local server
 // Will use io.Copy - http://golang.org/pkg/io/#Copy
-func handleClient(client net.Conn, remote net.Conn) {
+func handleClient(client net.Conn, local net.Conn) {
 	defer client.Close()
 	chDone := make(chan bool)
 
-	// Start remote -> local data transfer
+	// Start local -> client data transfer
 	go func() {
-		_, err := io.Copy(client, remote)
+		_, err := io.Copy(client, local)
 		if err != nil {
 			if el := log.Debug(); el.Enabled() {
-				el.Err(err).Msg("Error copying remote->local:")
+				el.Err(err).Msg("Error copying local -> client:")
 			}
 		}
 		chDone <- true
 	}()
 
-	// Start local -> remote data transfer
+	// Start client -> local data transfer
 	go func() {
-		_, err := io.Copy(remote, client)
+		_, err := io.Copy(local, client)
 		if err != nil {
 			if el := log.Debug(); el.Enabled() {
-				el.Err(err).Msg("Error copying local->remote:")
+				el.Err(err).Msg("Error copying client -> local:")
 			}
 		}
 		chDone <- true
